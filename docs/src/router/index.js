bf8e85a4039a06b8ad806ffe9af01bead547d5bc
Luke Purton <luke.purton@gmail.com>
2017-06-20 10:31:55 +1000

    Remove prop-mutate-test

--- docs/src/router/index.js
+++ docs/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home'
-import PropMutateTest from '@/pages/prop-mutate-test'
 
 Vue.use(Router)
 
@@ -11,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    },
-    {
-      path: '/prop-mutate-test',
-      name: 'PropMutateTest',
-      component: PropMutateTest
     }
   ]
 })
